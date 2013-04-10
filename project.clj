64e229dc4c4bbb3721b15016b2f079b6c88f581c
Yannick Scherer <yannick.scherer@gmail.com>
2013-04-10 02:48:43 +0200

    Added log4j

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.reflections/reflections "0.9.9-RC1"]
                  [org.apache.thrift/libthrift "0.9.0"]
-                 [javax.servlet/servlet-api "2.5"]]
+                 [javax.servlet/servlet-api "2.5"]
+                 [potemkin "0.2.2-SNAPSHOT"]]
+  :profiles { :dev { :dependencies [[org.slf4j/slf4j-log4j12 "1.5.2"]] } }
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"])
