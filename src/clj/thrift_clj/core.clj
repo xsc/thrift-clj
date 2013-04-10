690d9198834ccd3eb4cd96d2983c3fddb0bd3ca2
Yannick Scherer <yannick.scherer@gmail.com>
2013-04-10 02:49:05 +0200

    Implemented 'defservice'.

--- src/clj/thrift_clj/core.clj
+++ src/clj/thrift_clj/core.clj
@@ -2,14 +2,33 @@
        :author "Yannick Scherer" }
   thrift-clj.core
   (:refer-clojure :exclude [load])
-  (:use thrift-clj.core.thrift-types))
+  (:use [potemkin :only [import-macro import-fn]])
+  (:require [thrift-clj.core.thrift-types :as t]
+            [thrift-clj.core.thrift-services :as s]))
 
-;; ## Main Macro
+;; ## Imported
+
+(import-macro s/defservice)
+(import-fn t/clj->thrift)
+(import-fn t/thrift->clj)
+
+;; ## Main Macros
+
+(defmacro load-types
+  "Load Thrift Types from the given packages."
+  [& packages]
+  (let [packages (map str packages)]
+    `(do ~@(t/generate-thrift-types packages) true)))
+
+(defmacro load-services
+  "Load Thrift Services from the given packages."
+  [& packages]
+  (let [packages (map str packages)]
+    `(do ~@(s/generate-thrift-services packages) true)))
 
 (defmacro load
   "Load all Thrift Entities in the given packages."
   [& packages]
-  (let [packages (map str packages)
-        type-definitions (generate-thrift-types packages)]
-    `(do
-       ~@type-definitions)))
+  `(do
+     (load-types ~@packages)
+     (load-services ~@packages)))
--- src/clj/thrift_clj/core/thrift_services.clj
+++ src/clj/thrift_clj/core/thrift_services.clj
@@ -3,4 +3,65 @@
   thrift-clj.core.thrift-services
   (:require [thrift-clj.reflect :as reflect]
             [thrift-clj.thrift :as thrift]
-            [thrift-clj.utils :as u]))
+            [thrift-clj.utils :as u])
+  (:use thrift-clj.core.thrift-types))
+
+;; ## Services
+;;
+;; Services are imported by creating wrapper functions that build the
+;; Client/Server objects, as well as TODO
+
+(defmulti map->processor
+  "Multimethod that can create an `org.apache.thrift.TProcessor` by dispatching
+   a map of method-keyword/function pairs using a fully qualified class name."
+  (fn [cls m] cls))
+
+(defmacro defservice
+  "Implement the given Service (a fully qualified class name)."
+  [id service-cls & implementation]
+  (let [m (reduce
+            (fn [m [id bindings & method-impl]]
+              (assoc m (keyword id) 
+                     `(fn [~@bindings] ~@method-impl)))
+            {} implementation)]
+    `(def ~id
+       (map->processor '~service-cls ~m))))
+
+;; ## Form Generation
+
+(defn- generate-thrift-processor
+  "Make a given Service accessible by `defservice` by implementing the multimethod
+   `map->processor` for the fully qualified class name given."
+  [n cls mth]
+  (let [proc (u/inner cls "Processor")
+        iface (u/inner cls "Iface")
+        param-syms (repeatedly gensym)
+        handler (gensym "handler-")
+        conv-sym (gensym)]
+    `(do
+       (defn- ~conv-sym
+         [~handler]
+         (new
+           ~proc
+           (proxy [~iface] []
+             ~@(for [{:keys[name params]} mth]
+                 (let [params (take (count params) param-syms)]
+                   `(~(symbol name) [~@params]
+                       (if-let [h# (get ~handler ~(keyword name))]
+                         (clj->thrift
+                           (h# ~@(map #(list `thrift->clj %) params)))
+                         (throw (Exception. ~(str "[Thrift] Service Method not implemented: " n "." name))))))))))
+       (defmethod map->processor '~cls
+         [~'_ m#]
+         (~conv-sym m#)))))
+
+(defn generate-thrift-services
+  [packages]
+  (let [services (reflect/thrift-services packages)]
+    (for [s services]
+      (let [n (reflect/class-symbol s)
+            cls (reflect/full-class-symbol s)
+            mth (reflect/thrift-service-methods s)
+            iface (gensym "iface-")]
+        `(do
+           ~(generate-thrift-processor n cls mth))))))
