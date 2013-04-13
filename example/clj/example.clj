f8ea8a4c86339cb26601f605c26d10dc103591cf
Yannick Scherer <yannick.scherer@gmail.com>
2013-04-14 00:13:34 +0200

    Extended Example.

--- example/clj/example.clj
+++ example/clj/example.clj
@@ -3,38 +3,42 @@
 
 ;; Import Thrift Classes
 (thrift/import
-  (:types org.example.Person)
+  (:types [org.example Person Name])
   (:services [org.example.PersonIndex :as PS])
   (:clients [org.example.PersonIndex :as PI]))
 
 ;; Implement the Service
 (thrift/defservice person-index-service PS
   :let [person-db (atom {})]
-  (store [{:keys[firstName lastName age] :as p}]
+  (store [{:keys[name age id] :as p}]
     (println "Storing Person:")
-    (println "  First Name:" firstName)
-    (println "  Last Name:" lastName)
+    (println "  First Name:" (:firstName name))
+    (println "  Last Name:" (:lastName name))
     (println "  Age:" age)
-    (swap! person-db update-in [lastName] conj p) 
+    (swap! person-db assoc id p) 
     true)
-  (getPerson [lastName]
-    (first (@person-db lastName))))
+  (getPerson [id]
+    (get @person-db id)))
 
 ;; Start a Server
-(def server (thrift/single-threaded-server person-index-service :socket 7007))
-(future (thrift/start-server! server))
 
 ;; Prepare Client and Data
 (def client (thrift/create-client PI :socket "localhost" 7007))
-(def p (Person. "Some" "One" 99))
+(def p (Person. 0 (Name. "Some" "One") 99))
+;;(def tp (thrift/->thrift p)))
+;;(thrift/->clj tp)
 
 ;; Go!
-(with-open [c (thrift/connect! client)]
-  (PI/store c p)
-  (println "Trying to retrieve Person ...")
-  (let [p' (PI/getPerson c "One")]
-    (println p' (if (= p p') "[matches sent Person]" "")))
-  (println "Done."))
-
-;; Cleanup
-(thrift/stop-server! server)
+(defn -run
+  []
+  (when-let [server (thrift/multi-threaded-server person-index-service :socket 7007)]
+    (try
+      (future (thrift/start-server! server))
+      (with-open [c (thrift/connect! client)]
+        (PI/store c p)
+        (println "Trying to retrieve Person ...")
+        (let [p' (PI/getPerson c (int 0))]
+          (println p' (if (= p p') "[matches sent Person]" "")))
+        (println "Done."))
+      (finally
+        (thrift/stop-server! server)))))
--- example/thrift/test.thrift
+++ example/thrift/test.thrift
@@ -1,12 +1,17 @@
 namespace java org.example
 
-struct Person {
+struct Name {
   1: optional string firstName,
-  2: string lastName,
+  2: string lastName
+}
+
+struct Person {
+  1: i32 id
+  2: Name name,
   3: byte age
 }
 
 service PersonIndex {
-    bool store(1:Person p),
-    Person getPerson(1:string lastName)
+    void store(1:Person p),
+    Person getPerson(1:i32 id)
 }
