843e061349eb32cf5ca8c6c464ebeab656513444
Yannick Scherer <yannick.scherer@gmail.com>
2013-05-20 11:23:41 +0200

    Back to SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thrift-clj "0.1.1"
+(defproject thrift-clj "0.1.1-SNAPSHOT"
   :description "Clojure and Thrift working hand in hand."
   :url "https://github.com/xsc/thrift-clj"
   :license {:name "Eclipse Public License"
