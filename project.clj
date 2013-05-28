43e4e67de6390a43a7741ea560453d8cba9410ca
Yannick Scherer <yannick.scherer@gmail.com>
2013-05-28 12:31:17 +0200

    Testing against Clojure 1.4, 1.5.1 and 1.6.0-master-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thrift-clj "0.1.1-SNAPSHOT"
+(defproject thrift-clj "0.1.2-SNAPSHOT"
   :description "Clojure and Thrift working hand in hand."
   :url "https://github.com/xsc/thrift-clj"
   :license {:name "Eclipse Public License"
@@ -11,20 +11,24 @@
                  [javax.servlet/servlet-api "2.5"]
                  [org.slf4j/slf4j-log4j12 "1.5.2"]
                  [potemkin "0.2.2"]]
+  :repositories  {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+  :exclusions [org.clojure/clojure]
 
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.0.1"]]}
+  :profiles {:test {:dependencies [[midje "1.5.1"]]
+                    :plugins [[lein-midje "3.0.1"]
+                              [lein-thriftc "0.1.0"]]
+                    :prep-tasks ["thriftc"]
+                    :test-paths ["test-thrift/clj"]
+                    :thriftc {:source-paths ["test-thrift/thrift"]}}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :doc {:plugins [[codox "0.6.4"]
                              [lein-marginalia "0.7.1"]]
                    :codox {:sources ["src/thrift_clj"]
                            :output-dir "doc/autodoc"}}
-             :test-reflection { :warn-on-reflection true }
-             :test-all {:plugins [[lein-thriftc "0.1.0"]]
-                        :prep-tasks ["thriftc"]
-                        :test-paths ["test-thrift/clj"]
-                        :thriftc {:source-paths ["test-thrift/thrift"]}}}
+             :reflection {:warn-on-reflection true}}
 
-  :aliases {"midje-all" ["with-profile" "dev,test-all" "midje"]
-            "midje-all+reflection" ["with-profile" "dev,test-all,test-reflection" "midje"]
+  :aliases {"midje-all" ["with-profile" "test,1.4:test,1.5:test,1.6" "midje"]
             "doc-marginalia" ["with-profile" "doc" "marg" "-d" "doc/autodoc" "-f" "marginalia.html"]
             "doc-codox" ["with-profile" "doc" "doc"]})
