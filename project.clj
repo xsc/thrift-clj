389233c3ef17340a952f4ab69a581b97e98dbf90
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-11-18 00:02:34 +0100

    bump SNAPSHOT to minor.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thrift-clj "0.2.2-SNAPSHOT"
+(defproject thrift-clj "0.3.0-SNAPSHOT"
   :description "Clojure and Thrift working hand in hand."
   :url "https://github.com/xsc/thrift-clj"
   :license {:name "Eclipse Public License"
