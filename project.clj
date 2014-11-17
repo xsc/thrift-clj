fe789bf9591aa81abd2382e7fe9bd8841c71e8c6
Yannick Scherer <yannick.scherer@stylefruits.de>
2014-11-17 23:02:15 +0100

    upgrade dependencies and plugins.

--- project.clj
+++ project.clj
@@ -4,25 +4,26 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [org.reflections/reflections "0.9.9-RC1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.reflections/reflections "0.9.9"]
                  [org.apache.thrift/libthrift "0.9.1" :exclusions [org.slf4j/slf4j-api]]
                  [javax.servlet/servlet-api "2.5"]
-                 [potemkin "0.3.2"]]
+                 [potemkin "0.3.11"]]
   :repositories  {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :exclusions [org.clojure/clojure]
 
-  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.0.13"]]}
-             :test {:dependencies [[midje "1.5.1"]]
-                    :plugins [[lein-midje "3.0.1"]
-                              [lein-thriftc "0.1.0"]]
-                    :prep-tasks ["thriftc"]
+  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.1.2"]]}
+             :test {:dependencies [[midje "1.6.3"]]
+                    :plugins [[lein-midje "3.1.3"]
+                              [lein-thriftc "0.2.1"]]
+                    :hooks [leiningen.thriftc]
                     :test-paths ["test-thrift/clj"]
+                    :javac-opts ["-source" "1.6" "-target" "1.6"]
                     :thriftc {:source-paths ["test-thrift/thrift"]}}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :doc {:plugins [[codox "0.6.4"]
                              [lein-marginalia "0.7.1"]]
                    :codox {:sources ["src/thrift_clj"]
