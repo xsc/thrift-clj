109c442fc62301cdbe484cfd4d472b6a095e4804
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-09-25 11:32:38 +0200

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -6,17 +6,17 @@
             :year 2013
             :key "mit"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.reflections/reflections "0.9.9"]
-                 [org.apache.thrift/libthrift "0.9.1" :exclusions [org.slf4j/slf4j-api]]
+                 [org.reflections/reflections "0.9.10"]
+                 [org.apache.thrift/libthrift "0.9.2" :exclusions [org.slf4j/slf4j-api]]
                  [javax.servlet/servlet-api "2.5"]
-                 [potemkin "0.3.11"]]
+                 [potemkin "0.4.1"]]
   :repositories  {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :exclusions [org.clojure/clojure]
 
-  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.1.2"]
-                                  [midje "1.6.3"]]
+  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.1.3"]
+                                  [midje "1.7.0"]]
                    :plugins [[lein-midje "3.1.3"]
                              [lein-thriftc "0.2.1"]]
                    :hooks [leiningen.thriftc]
@@ -25,7 +25,7 @@
                    :thriftc {:source-paths ["test-thrift/thrift"]}}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :doc {:plugins [[codox "0.6.4"]
                              [lein-marginalia "0.7.1"]]
                    :codox {:sources ["src/thrift_clj"]
