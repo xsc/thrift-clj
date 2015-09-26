0e94b2f7a092257033ba50a56f6ea73798ce5681
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-09-26 13:39:28 +0200

    upgrade plugins.

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
   :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.1.3"]
                                   [midje "1.7.0"]]
                    :plugins [[lein-midje "3.1.3"]
-                             [lein-thriftc "0.2.1"]]
+                             [lein-thriftc "0.2.3"]]
                    :hooks [leiningen.thriftc]
                    :test-paths ["test-thrift/clj"]
                    :javac-opts ["-source" "1.6" "-target" "1.6"]
@@ -26,13 +26,10 @@
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :doc {:plugins [[codox "0.6.4"]
-                             [lein-marginalia "0.7.1"]]
-                   :codox {:sources ["src/thrift_clj"]
-                           :output-dir "doc/autodoc"}}
+             :doc {:plugins [[codox "0.8.13"]]
+                   :codox {:sources ["src/thrift_clj"]}}
              :reflection {:warn-on-reflection true}}
 
   :aliases {"all" ["with-profile" "+1.5:+1.6:+1.7"]
             "test" "midje"
-            "doc-marginalia" ["with-profile" "doc" "marg" "-d" "doc/autodoc" "-f" "marginalia.html"]
-            "doc-codox" ["with-profile" "doc" "doc"]})
+            "doc" ["with-profile" "doc" "doc"]})
